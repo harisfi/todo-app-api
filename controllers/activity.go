108e964bb62641193d92ce33dae8db2c1c271bcd
harisfi <acc.cp.haris@gmail.com>
2021-12-24 23:52:06 +0700

    Add CRUD functionality to activity entity

--- controllers/activity.go
+++ controllers/activity.go
@@ -1,28 +1,102 @@
 package controllers
 
-import "github.com/valyala/fasthttp"
+import (
+	"todo-app-api/database"
+	"todo-app-api/database/models"
+	"todo-app-api/services"
+
+	"github.com/valyala/fasthttp"
+)
+
+func findOneActivity(ctx *fasthttp.RequestCtx) (models.Activity, interface{}) {
+	id := ctx.UserValue("id")
+	activity := models.Activity{}
+
+	database.GetDB().Find(&activity, id)
+	return activity, id
+}
 
 func GetAllActivity(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json")
-	ctx.SetBody([]byte("GetAllActivity: you're in " + string(ctx.RequestURI())))
+	activities := []models.Activity{}
+	database.GetDB().Find(&activities)
+	services.SendJSONResponse(ctx, activities, "", "")
 }
 
 func GetOneActivity(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json")
-	ctx.SetBody([]byte("GetOneActivity: you're in " + string(ctx.RequestURI())))
+	activity, id := findOneActivity(ctx)
+	if activity.ID == 0 {
+		ctx.SetStatusCode(404)
+		services.SendJSONResponse(ctx, nil, "Not Found", "Activity with ID " + id.(string) + " Not Found")
+	} else {
+		services.SendJSONResponse(ctx, activity, "", "")
+	}
 }
 
 func CreateActivity(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json")
-	ctx.SetBody([]byte("CreateActivity: you're in " + string(ctx.RequestURI())))
+	req := services.ExtractRequests(ctx)
+
+	var title, email string
+
+	if req["title"] == nil {
+		ctx.SetStatusCode(400)
+		services.SendJSONResponse(ctx, nil, "Bad Request", "title cannot be null")
+		return
+	}
+	title = req["title"].(string)
+
+	if req["email"] == nil {
+		email = ""
+	} else {
+		email = req["email"].(string)
+	}
+
+	a := models.Activity{
+		Title: title,
+		Email: email,
+	}
+
+	database.GetDB().Create(&a)
+	services.SendJSONResponse(ctx, a, "", "")
 }
 
 func DeleteActivity(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json")
-	ctx.SetBody([]byte("DeleteActivity: you're in " + string(ctx.RequestURI())))
+	activity, id := findOneActivity(ctx)
+	if activity.ID == 0 {
+		ctx.SetStatusCode(404)
+		services.SendJSONResponse(ctx, nil, "Not Found", "Activity with ID " + id.(string) + " Not Found")
+	} else {
+		database.GetDB().Delete(&activity)
+		services.SendJSONResponse(ctx, nil, "", "")
+	}
 }
 
 func UpdateActivity(ctx *fasthttp.RequestCtx) {
-	ctx.SetContentType("application/json")
-	ctx.SetBody([]byte("UpdateActivity: you're in " + string(ctx.RequestURI())))
+	activity, id := findOneActivity(ctx)
+	if activity.ID == 0 {
+		ctx.SetStatusCode(404)
+		services.SendJSONResponse(ctx, nil, "Not Found", "Activity with ID " + id.(string) + " Not Found")
+	} else {
+		req := services.ExtractRequests(ctx)
+
+		var title, email string
+
+		if req["title"] == nil {
+			ctx.SetStatusCode(400)
+			services.SendJSONResponse(ctx, nil, "Bad Request", "title cannot be null")
+			return
+		}
+		title = req["title"].(string)
+
+		if req["email"] == nil {
+			email = ""
+		} else {
+			email = req["email"].(string)
+		}
+
+		activity.Title = title
+		activity.Email = email
+
+		database.GetDB().Updates(&activity)
+		services.SendJSONResponse(ctx, activity, "", "")
+	}
 }
